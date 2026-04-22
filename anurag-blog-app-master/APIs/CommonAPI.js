cc65f6475d83c611716608a22a242e501616eece
sudha-210705 <23eg105e56@anurag.edu.in>
2026-04-22 22:31:18 +0530

    fixed cookie sameSite and secure for production

--- anurag-blog-app-master/APIs/CommonAPI.js
+++ anurag-blog-app-master/APIs/CommonAPI.js
@@ -14,8 +14,8 @@ commonRouter.post("/login", async (req, res) => {
   //save tokan as httpOnly cookie
   res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    sameSite: "none",
+    secure: true,
   });
   //send res
   res.status(200).json({ message: "login success", payload: user });
@@ -26,8 +26,8 @@ commonRouter.get("/logout", (req, res) => {
   // Clear the cookie named 'token'
   res.clearCookie("token", {
     httpOnly: true, // Must match original  settings
-    secure: false, // Must match original  settings
-    sameSite: "lax", // Must match original  settings
+    secure: true, // Must match original  settings
+    sameSite: "none", // Must match original  settings
   });
 
   res.status(200).json({ message: "Logged out successfully" });
